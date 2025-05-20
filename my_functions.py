4915e9f8e8d0088bfb21fd7a40790a47ddf96a1e
heyscottn <heyscottn@gmail.com>
2025-05-20 12:57:26 -0500

    mm

--- my_functions.py
+++ my_functions.py
@@ -23,24 +23,4 @@ def save(mat, filename):
 
 
 
-# given a string
-# return a list, which is 'binary' version
-# ex: [1, 0, 0, 1, 0, ect]
-def convert_to_list(s):
-    ans = []
-    # TODO
-    return ans
-
-
-   
-# given a list of 1s and 0s
-# replace the least significant bit of the red value
-# with the message
-# mess is a list of 0s and 1s, ex: [1,1,0,0,0,1,0,etc]
-# return the new matrix
-def hide_message(mat, mess):
-    # TODO
-    return mat
-
-
 
